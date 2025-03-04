a6526b5b403bffa62bc906b3e13772ddf2618682
Thunchanok-J <Thunchanok.Jp@gmail.com>
2025-03-04 23:45:11 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,35 +3,10 @@ import axios from "axios";
 import "./App.css";
 import catGif from "./image/cat.gif";
 
-class App extends React.Component {
+/*class App extends React.Component {
   state = { word: "", definition: "", partOfSpeech: "" };
 
-  // fetchAdvice = () => {
-  //   axios
-  //     .get("https://api.dictionaryapi.dev/api/v2/entries/en/recognition")
-  //     .then((response) => {
-  //       const word = response.data[0].word;
-  //       const definition =
-  //         response.data[0].meanings[0].definitions[0].definition;
-  //       let phonetic = response.data[0].phonetic;
-  //       if (!phonetic && response.data[0].phonetics.length > 0) {
-  //         phonetic =
-  //           response.data[0].phonetics.find((p) => p.text)?.text || "N/A";
-  //       }
-  //       console.log(word);
-  //       console.log(definition);
-  //       console.log(phonetic);
-  //       this.setState({ word, definition, phonetic });
-  //     })
-
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-  componentDidMount() {
-    this.fetchAdvice();
-  }
-  fetchAdvice = () => {
+  /*fetchWord = () => {
     axios
       .get("https://random-word-api.herokuapp.com/word?lang=en")
       .then((res) => {
@@ -41,11 +16,6 @@ class App extends React.Component {
         );
       })
       .then((response) => {
-        if (!response.data || response.data.length === 0) {
-          console.warn("Word not found, fetching again...");
-          this.fetchAdvice(); // เรียก fetchAdvice ใหม่
-          return;
-        }
         const word = response.data[0].word;
         const definition =
           response.data[0].meanings[0].definitions[0].definition;
@@ -60,10 +30,13 @@ class App extends React.Component {
 
         this.setState({ word, definition, partOfSpeech });
       })
-      .catch((error) => console.error("Error fetching word:", error));
-  };
+      .catch((error) => {
+        console.error("Error fetching word:", error);
+        this.fetchWord();
+      });
+  };*/
 
-  render() {
+/*render() {
     const { word } = this.state;
     const { definition } = this.state;
     const { partOfSpeech } = this.state;
@@ -77,7 +50,85 @@ class App extends React.Component {
               {partOfSpeech}
             </h6>{" "}
             <p className="card-text">{definition}</p>{" "}
-            <button type="button" className="button" onClick={this.fetchAdvice}>
+            <button type="button" className="button" onClick={this.fetchWord}>
+              <img src={catGif} alt="Cat" />
+              <span className="thaifont">random</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}*/
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      word: "",
+      definition: "",
+      partOfSpeech: "",
+    };
+    this.wordCache = new Map(); // ใช้ Map เก็บคำศัพท์ที่เคยเรียก
+  }
+
+  fetchWord = () => {
+    axios
+      .get("https://random-word-api.herokuapp.com/word?lang=en")
+      .then((res) => {
+        const randomWord = res.data?.[0];
+        if (!randomWord) throw new Error("Invalid word received");
+
+        // ถ้ามีคำนี้อยู่ใน Cache แล้ว ใช้ค่าที่เก็บไว้เลย
+        if (this.wordCache.has(randomWord)) {
+          console.log("Using cached word:", randomWord);
+          this.setState(this.wordCache.get(randomWord));
+          return;
+        }
+
+        return axios
+          .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`)
+          .then((response) => ({ response, randomWord }));
+      })
+      .then(({ response, randomWord }) => {
+        if (!response || !response.data || response.data.length === 0) {
+          throw new Error("Word not found in dictionary");
+        }
+
+        const wordData = response.data[0];
+        const word = wordData.word || "Unknown";
+        const definition =
+          wordData.meanings?.[0]?.definitions?.[0]?.definition ||
+          "No definition found.";
+        const partOfSpeech = wordData.meanings?.[0]?.partOfSpeech || "N/A";
+
+        console.log(word, definition, partOfSpeech);
+
+        // บันทึกคำนี้ลง Cache
+        this.wordCache.set(word, { word, definition, partOfSpeech });
+
+        this.setState({ word, definition, partOfSpeech });
+      })
+      .catch((error) => {
+        console.error("Error fetching word:", error);
+        this.fetchWord();
+      });
+  };
+
+  render() {
+    const { word, definition, partOfSpeech } = this.state;
+
+    return (
+      <div className="app">
+        <h1 className="title">Random Vocabulary</h1>
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h1 className="card-title">{word}</h1>
+            <h6 className="card-subtitle mb-2 text-body-secondary">
+              {partOfSpeech}
+            </h6>
+            <p className="card-text">{definition}</p>
+            <button type="button" className="button" onClick={this.fetchWord}>
               <img src={catGif} alt="Cat" />
               <span className="thaifont">random</span>
             </button>
@@ -87,4 +138,5 @@ class App extends React.Component {
     );
   }
 }
+
 export default App;
